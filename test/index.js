71a864ac64edf0e743e9745eb4c6a14532ce4c99
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-29 10:48:19 -0700

    support full [].slice semantics

--- test/index.js
+++ test/index.js
@@ -33,4 +33,43 @@ describe('sliced', function(){
       assert.equal(o[1], r[0]);
     })
   })
+  describe('with negative start and no end', function(){
+    it('begins at an offset from the end and includes all following elements', function(){
+      var o = [3, "4", 5, null];
+      var r = sliced(o, -2);
+      assert.equal(2, r.length);
+      assert.equal(o[2], r[0]);
+      assert.equal(o[3], r[1]);
+    })
+  })
+  describe('with negative start and positive end', function(){
+    it('begins at an offset from the end and includes `end` elements', function(){
+      var o = [3, "4", {x:1}, null];
+
+      var r = sliced(o, -2, 1);
+      assert.equal(0, r.length);
+
+      var r = sliced(o, -2, 2);
+      assert.equal(0, r.length);
+
+      var r = sliced(o, -2, 3);
+      assert.equal(1, r.length);
+      assert.equal(o[2], r[0]);
+    })
+  })
+  describe('with negative start and negative end', function(){
+    it('begins at `start` offset from the end and includes all elements up to `end` offset from the end', function(){
+      var o = [3, "4", {x:1}, null];
+      var r = sliced(o, -3, -1);
+      assert.equal(2, r.length);
+      assert.equal(o[1], r[0]);
+      assert.equal(o[2], r[1]);
+
+      var r = sliced(o, -3, -3);
+      assert.equal(0, r.length);
+
+      var r = sliced(o, -3, -4);
+      assert.equal(0, r.length);
+    })
+  })
 })
